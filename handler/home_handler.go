c5c5ac22659c2259334e91ae63a3668c871fbe19
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-03-06 21:14:27 +0100

    move handler

--- handler/home_handler.go
+++ handler/home_handler.go
@@ -1,10 +1,29 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/gchumillas/ucms/manager"
 )
+
+func (env *Env) SignIn(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Username string
+		Password string
+	}
+	parseBody(w, r, &body)
+
+	u := manager.NewUser()
+	if !u.ReadUserByCredentials(env.DB, body.Username, body.Password) {
+		httpError(w, docNotFoundError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(u.NewToken(env.PrivateKey, env.Expiration))
+}
 
 func (env *Env) Home(w http.ResponseWriter, r *http.Request) {
 	u := getUser(r)
