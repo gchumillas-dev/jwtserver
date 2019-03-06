2759888d6d32fbf0ae312783784bd7af45522900
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-03-06 21:13:07 +0100

    home controller: show a welcome message

--- handler/home_handler.go
+++ handler/home_handler.go
@@ -1,9 +1,14 @@
 package handler
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 )
 
 func (env *Env) Home(w http.ResponseWriter, r *http.Request) {
 	u := getUser(r)
+
+	msg := fmt.Sprintf("Hello %s!\nYou are ahthorized.", u.Username)
+	io.WriteString(w, msg)
 }
